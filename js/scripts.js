613a57dcd50c3d3a650383b60f60ee8a657003e3
Jieun Kang and Rachel Schieferstein <jieun+rachel@>
2020-01-21 15:38:45 -0800

    add function for listed output

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,26 @@ $(document).ready(function() {
   var numbers = ["ace",2,3,4,5,6,7,8,9,10,"jack", "queeen", "king"]
   var suits = ["clubs", "diamonds", "hearts", "spades"]
   var decks = [];
+  
 
   suits.forEach(function(suit) { 
     numbers.forEach(function(number) {
-      decks.push(number + " of " + suit);
-    })
+      tempString = temp(number + " of "+ suit);
+      decks.push(tempString);
+  }
+);
   console.log(decks);  
   
-  $(".output").each(function (index) { 
-    $(this).text(decks[index]);
+  // $(".output").each(function (index) { 
+  //   $(this).text(decks[index]);
     
-  $(".output").text("<li>" + decks + "</li><br>");
-  });
+  $(".output").html(decks);
+
+  function temp(value)
+  {
+    return ("<li>"+value+"</li>")
+  }
 
 });
+
 });
